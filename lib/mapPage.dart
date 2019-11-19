0fad446c4bde35f5dde78a143415b74850b23533
Alex <up201705049@fe.up.pt>
2019-11-19 00:15:17 +0000

    Maps working with GoogleMaps

--- lib/mapPage.dart
+++ lib/mapPage.dart
@@ -1,12 +1,13 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:matrix_gesture_detector/matrix_gesture_detector.dart';
 import 'User.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 
 class MapPage extends StatefulWidget {
   final AssetImage _map = AssetImage("lib/assets/images/FEUPmap.png");
-  User user = new User(10, 10);
+  final User user = new User(10, 10);
 
   @override
   State<StatefulWidget> createState() {
@@ -15,7 +16,11 @@ class MapPage extends StatefulWidget {
 }
 
 class MapController extends State<MapPage> {
-  Matrix4 _matrix = Matrix4.translationValues(0, 0, 0);
+  Completer<GoogleMapController> _controller = Completer();
+  static final CameraPosition _feupPosition = CameraPosition(
+    target: LatLng(41.177764, -8.596490),
+    zoom: 14.4746,
+  );
   final AssetImage _map;
   User _user;
 
@@ -25,31 +30,21 @@ class MapController extends State<MapPage> {
   Widget build(BuildContext context) { //"draw" method
     return Container(
       alignment: Alignment.center,
-        child: SizedBox.expand(
-            child: MatrixGestureDetector(
-                onMatrixUpdate: (Matrix4 m, Matrix4 tm, Matrix4 sm, Matrix4 rm) {
-                  setState(() {
-                    _matrix = m;
-                  });
-                },
-                child: Transform(
-                    transform: _matrix,
-                    child: Stack(
-                      children: <Widget>[
-                        //Background Map
-                        Image(image: _map),
-                        //List of users overlay
-                        //...
-                        //Main User Overlay
-                        Positioned(
-                          bottom: _user.yCoord + 50,
-                          left: _user.xCoord + 50,
-                          child: Icon(FontAwesomeIcons.solidSurprise),
-                        )
-                      ],
-                    )
-                )
+        child: Stack(
+          children: <Widget>[
+            GoogleMap(
+              mapType: MapType.normal,
+              initialCameraPosition: _feupPosition,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+            ),
+            Positioned(
+              bottom: _user.yCoord + 50,
+              left: _user.xCoord + 50,
+              child: Icon(FontAwesomeIcons.solidSurprise),
             )
+          ],
         )
     );
   }
