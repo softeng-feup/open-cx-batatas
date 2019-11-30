8c2e98428daa5e0493ab9f531c3be0373097ab88
Christopher Abreu <up201604735@fe.up.pt>
2019-11-30 01:41:48 +0000

    Added appBar to event page

--- frontend/lib/eventsPage.dart
+++ frontend/lib/eventsPage.dart
@@ -4,16 +4,108 @@ class EventsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //"draw" method
-    return Container(
-      child: Row(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Center(),
-            ],
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Flexible(
+          child: Container(
+            height: 80.0,
+            color: Colors.grey,
+            child: Row(
+              children: <Widget>[
+                Flexible(
+                    child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 9,
+                      child: Container(
+                          child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Flexible(
+                            child: Row(
+                             children: <Widget>[
+                               Expanded(child: Text('  Mon',textAlign: TextAlign.center)),
+                               Expanded(child: Text(' Tue',textAlign: TextAlign.center)),
+                               Expanded(child: Text(' Wen',textAlign: TextAlign.center)),
+                               Expanded(child: Text(' Thu',textAlign: TextAlign.center))
+
+                             ],
+                            ),
+                          ),
+                          Flexible(
+                            child: Container(
+                              margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(50.0),
+                                  color: Colors.blue),
+                              child: Row(
+                                children: <Widget>[
+                                  Expanded(
+                                    child: Container(
+                                      child: RawMaterialButton(
+                                        onPressed: () {},
+                                        shape: new CircleBorder(),
+                                        fillColor: Colors.white,
+                                        child: Text('23'),
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Container(
+                                      child: RawMaterialButton(
+                                        onPressed: () {},
+                                        shape: new CircleBorder(),
+                                        fillColor: Colors.orangeAccent,
+                                        child: Text('24'),
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Container(
+                                      child: RawMaterialButton(
+                                        onPressed: () {},
+                                        shape: new CircleBorder(),
+                                        fillColor: Colors.white,
+                                        child: Text('25'),
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Container(
+                                      child: RawMaterialButton(
+                                        onPressed: () {},
+                                        shape: new CircleBorder(),
+                                        fillColor: Colors.white,
+                                        child: Text('26'),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      )),
+                    ),
+                    Expanded(
+                        flex: 2,
+                        child: Container(
+                          alignment: AlignmentDirectional.centerEnd,
+                          child: RawMaterialButton(
+                            onPressed: () {},
+                            shape: new CircleBorder(),
+                            fillColor: Colors.orangeAccent,
+                            child: Text('Add+'),
+                          ),
+                        )),
+                  ],
+                )),
+              ],
+            ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
