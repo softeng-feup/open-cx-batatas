d3ff088a561571cbe5cb46e56721dd785e6a2b7e
Diogo Silva <its.diogo.silva@gmail.com>
2019-12-17 15:25:27 +0000

    Removed AppBar from AllEvents screen

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -12,6 +12,7 @@ import 'package:http/http.dart' as http;
 import 'Constants.dart' as Constants;
 import 'Auth.dart';
 import 'dart:io';
+import 'Classes.dart';
 
 import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
@@ -66,12 +67,43 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _currentIndex = 0;
-  final List<Widget> _children = [
-    MapPage(),
-    //EventsPage(),
-    AllEventsPage(),
-    ProfilePage(),
-  ];
+
+  static bool eventsReady = false;
+  static List<Event> events = new List();
+  static List<int> bookmarkIds = new List();
+  static List<Event> list23 = new List();
+  static List<Event> list24 = new List();
+  static List<Event> list25 = new List();
+  static List<Event> list26 = new List();
+
+  List<Widget> _children() {
+    return [
+      MapPage(),
+      //EventsPage(),
+      AllEventsPage(
+        eventsReady: eventsReady,
+        events: events,
+        bookmarkIds: bookmarkIds,
+        list23: list23,
+        list24: list24,
+        list25: list25,
+        list26: list26,
+        updateBookmark: updateBookmark,
+      ),
+      ProfilePage(),
+    ];
+  }
+
+  String getNavTitle() {
+    switch (_currentIndex) {
+      case 1:
+        return 'All Events';
+      case 2:
+        return 'Profile';
+      default:
+        return widget.title;
+    }
+  }
 
   /*
      0 - loading
@@ -87,6 +119,49 @@ class _MyHomePageState extends State<MyHomePage> {
     currentState = 0;
     hasSeenOnboarding = false;
     fetchAppState();
+    fetchEvents();
+  }
+
+  /* Updates bookmarks information from storage */
+  Future updateBookmarksFromStorage() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final path = directory.path;
+    File bookmarksFile = File('$path/bookmarks.txt'); // TODO: add to Constants
+
+    try {
+      String bookmarksFileContents = await bookmarksFile.readAsString();
+      Map<String, dynamic> bookmarksJson = json.decode(bookmarksFileContents);
+
+      int numBookmarks = bookmarksJson['bookmarks'].length;
+      for (var i = 0; i < numBookmarks; i++) {
+        bookmarkIds.add(bookmarksJson['bookmarks'][i]);
+      }
+    } catch (e) {
+      return null;
+    }
+  }
+
+  /* Saves bookmarks information to storage */
+  void saveBookmarksToStorage() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final path = directory.path;
+    File bookmarksFile = File('$path/bookmarks.txt'); // TODO: add to Constants
+    bookmarksFile.writeAsString(json.encode({'bookmarks': bookmarkIds}));
+  }
+
+  void updateBookmark({int eventId, bool isBookmarked}) {
+    setState(() {
+      if (isBookmarked) {
+        if (!bookmarkIds.contains(eventId)) {
+          bookmarkIds.add(eventId);
+        }
+      } else {
+        if (bookmarkIds.contains(eventId)) {
+          bookmarkIds.remove(eventId);
+        }
+      }
+      saveBookmarksToStorage();
+    });
   }
 
   /* Gets token information from storage */
@@ -118,22 +193,67 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  void fetchEvents() async {
+    final response = await http.get(
+        'http://diogo98s.pythonanywhere.com/api/v1/events/',
+        headers: {'Content-Type': 'application/json'});
+
+    if (response.statusCode == 200) {
+      var utf8Data = utf8.decode(response.bodyBytes);
+      var numEvents = jsonDecode(utf8Data).length;
+
+      await updateBookmarksFromStorage();
+
+      for (var i = 0; i < numEvents; i++) {
+        Map<String, dynamic> jsonData = json.decode(response.body)[i];
+
+        if (bookmarkIds.contains(jsonData['id'])) {
+          jsonData['is_bookmarked'] = true;
+        } else {
+          jsonData['is_bookmarked'] = false;
+        }
+
+        events.add(Event.fromJson(jsonData));
+      }
+
+      setState(() {
+        buildDayLists();
+        eventsReady = true;
+      });
+    } else {
+      throw Exception('Failed to fetch events');
+    }
+  }
+
+  void buildDayLists() {
+    for (var i = 0; i < events.length; i++) {
+      var dateParsedS = DateTime.parse(events[i].startTime);
+
+      if (dateParsedS.day == 23) {
+        list23.add(events[i]);
+      }
+      if (dateParsedS.day == 24) {
+        list24.add(events[i]);
+      }
+      if (dateParsedS.day == 25) {
+        list25.add(events[i]);
+      }
+      if (dateParsedS.day == 26) {
+        list26.add(events[i]);
+      }
+    }
+  }
+
   Scaffold get normalScreen {
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title, style: TextStyle(fontFamily: 'Fontin Sans')),
+        title: Text(getNavTitle(), style: TextStyle(fontFamily: 'Fontin Sans')),
       ),
-      // body: _children[_currentIndex],
       body: IndexedStack(
         index: _currentIndex,
-        children: _children,
-      ),
-      // body: _children[_currentIndex],
-      body: IndexedStack(
-        index: _currentIndex,
-        children: _children,
+        children: _children(),
       ),
       bottomNavigationBar: BottomNavigationBar(
         onTap: changePage,
@@ -154,10 +274,6 @@ class _MyHomePageState extends State<MyHomePage> {
           BottomNavigationBarItem(
             icon: new Icon(Icons.person),
             title: new Text('Profile'),
-          ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.calendar_today),
-            title: new Text('Events'),
           ),
         ],
       ),
